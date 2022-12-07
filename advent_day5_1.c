b6607fd3a60ccf8a496cb7461060495a056238bd
David <omgemjey@gmail.com>
2022-12-07 16:33:54 +0100

    day5 fixed

--- advent_day5_1.c
+++ advent_day5_1.c
@@ -1,22 +1,5 @@
 #include "get_next_line.h"
 
-int	ft_power(int nb, int power)
-{
-	int i;
-	int result;
-
-	i = 0;
-	result = 1;
-	if (power < 0)
-		return (0);
-	while (i < power)
-	{
-		result *= nb;
-		i++;
-	}
-	return (result);
-}
-
 int	atoi_str(char *line)
 {
 	int i;
@@ -58,7 +41,7 @@ int	atoi_src(char *line)
 	int num;
 	int len;
 
-	len = ft_strlen(line) - 8;
+	len = ft_strlen(line) - 8; //change to "-7" on linux
 	num = line[len] - '0';
 	return (num);
 }
@@ -68,7 +51,7 @@ int	atoi_dst(char *line)
 	int num;
 	int len;
 
-	len = ft_strlen(line) - 3;
+	len = ft_strlen(line) - 3; //change to "-2" on linux
 	num = line[len] - '0';
 	return (num);
 }
@@ -82,38 +65,63 @@ int	get_next_movement(const char *str)
 	return (moves);
 }
 
+int	tower_last_index(char **tower)
+{
+	int i;
+
+	i = 0;
+	while (tower[i] != NULL)
+		i++;
+	return (i);
+}
+
 
 int	main(void)
 {
 	char	*line;
-	/*char	*tower0[20] = {"B", "S", "J", "Z", "V", "D", "G"};
-	char	*tower1[20] = {"P", "V", "G", "M", "S", "Z"};
-	char	*tower2[20] = {"F", "Q", "T", "W", "S", "B", "L", "C"};
-	char	*tower3[20] = {"Q", "V", "R", "M", "W", "G", "J", "H"};
-	char	*tower4[20] = {"D", "M", "F", "N", "S", "L", "C"};
-	char	*tower5[20] = {"D", "C", "G", "R"};
-	char	*tower6[20] = {"Q", "S", "D", "J", "R", "T", "G", "H"};
-	char	*tower7[20] = {"V", "F", "P"};
-	char	*tower8[20] = {"J", "T", "S", "R", "D"};*/
-	int		i;
-	int		fd1;
+	char	*tower0[60] = {"G", "D", "V", "Z", "J", "S", "B"};
+	char	*tower1[60] = {"Z", "S", "M", "G", "V", "P"};
+	char	*tower2[60] = {"C", "L", "B", "S", "W", "T", "Q", "F"};
+	char	*tower3[60] = {"H", "J", "G", "W", "M", "R", "V", "Q"};
+	char	*tower4[60] = {"C", "L", "S", "N", "F", "M", "D"};
+	char	*tower5[60] = {"R", "G", "C", "D"};
+	char	*tower6[60] = {"H", "G", "T", "R", "J", "D", "S", "Q"};
+	char	*tower7[60] = {"P", "F", "V"};
+	char	*tower8[60] = {"D", "R", "S", "T", "J"};
+	char	**towers[9] = {tower0, tower1, tower2, tower3, \
+		tower4, tower5, tower6, tower7, tower8};
+	char	*temp;
+	int		i = 1;
+	int		fd1 = open("shipping.txt", O_RDONLY);
 	int		moves;
+	int		size;
+	int		size2;
 	int		src;
 	int		dst;
-	
-	i = 1;
-	fd1 = open("shipping.txt", O_RDONLY);
-	while (i <= 5)
+	while (i <= 502)
 	{
 		line = get_next_line(fd1);
-		printf("%s", line);
 		moves = atoi_str(line);
-		src = atoi_src(line);
-		dst = atoi_dst(line);
-		printf("%d\n", moves);
-		printf("%d\n", src);
-		printf("%d\n", dst);
+		src = atoi_src(line) - 1;
+		dst = atoi_dst(line) - 1; 
+		size = tower_last_index(towers[src]);
+		size2 = tower_last_index(towers[dst]);
+		while (moves != 0)
+		{
+			temp = towers[src][size - 1];
+			towers[dst][size2] = temp;
+			towers[src][size - 1] = NULL;
+			size2++;
+			moves--;
+			size--;
+		}
 		i++;
 	}
-
+	for (int i = 0; i < 9; i++)
+	{
+		printf("Tower %d: ", i + 1);
+		for (int j = 0; towers[i][j] != NULL; j++)
+			printf("%s ", towers[i][j]);
+		printf("\n");
+	}
 }
